17f019ffeb4c3751a66e0b01ec4c5f5546bb6f09
Ferraresir <ramiro_ferraresi@hotmail.com>
2020-12-07 18:05:50 -0300

    version 1.44

--- src/backend/firebase/products.js
+++ src/backend/firebase/products.js
@@ -2,7 +2,7 @@ import { getFirestore } from './index';
 
 export const getProducts = (cat) => {
     return new Promise((resolve, reject) => {
-        let products = getFirestore().collection('productos').limit(8)
+        let products = getFirestore().collection('productos').limit(12)
         if (cat) products = products.where("category_id", "==", `${cat}`)
         products.get().then((query) => {
             if (query.size === 0) reject('no hay registros')

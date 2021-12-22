bec0aad6abf87a2ccb93154ec252155675a651f6
Ramiro Ferraresi <ramiro_ferraresi@hotmail.com>
2021-12-22 02:46:38 -0300

    remove firebase data

--- src/backend/firebase/index.js
+++ src/backend/firebase/index.js
@@ -3,14 +3,7 @@ import 'firebase/firestore';
 import 'firebase/firebase-auth'
 
 const app = firebase.initializeApp(
-    {
-        apiKey: "AIzaSyCIjYUieu4wNxnFJt8nJ3zSF6e8HxChVCM",
-        authDomain: "codermarket-5e610.firebaseapp.com",
-        projectId: "codermarket-5e610",
-        storageBucket: "codermarket-5e610.appspot.com",
-        messagingSenderId: "1027980225061",
-        appId: "1:1027980225061:web:68f42b30d00b1ca1dfadcc"
-    }
+   
 )
 
 export const getFirebase = () => {
